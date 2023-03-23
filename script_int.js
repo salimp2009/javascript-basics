003fc0abecf32e3cf94360cc99ab2a77a58e4093
salimp2009@gmail.com <salimp2009@gmail.com>
2023-03-23 16:24:23 +0300

    adding more examples

--- script_int.js
+++ script_int.js
@@ -12,3 +12,23 @@ console.log("total:", total);
 console.log("total:", total.toFixed(3));
 console.log("total:", total.toPrecision(2));
 console.log("total:", total.toLocaleString("en-US"));
+
+const text2 = "Map of NYC";
+
+if (text2.includes("NYC")) {
+  console.log(100);
+} else {
+  console.log(50);
+}
+
+const response = {
+  statusCode: 500,
+  ok: true,
+  data: [1, 2, 3],
+};
+
+if (!response.ok) {
+  console.log(response.statusCode);
+} else {
+  console.log(response.data);
+}
