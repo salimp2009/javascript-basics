aaa2669582f8eda1e2762e72790cd2e44787286a
salimp2009@gmail.com <salimp2009@gmail.com>
2023-03-28 16:12:00 +0300

    added innerHTML using `` backticks to be able to have multiple lines but displayed as the same line on the web page

--- script_dom.js
+++ script_dom.js
@@ -2,8 +2,25 @@ const buttonEl = document.querySelector(".btn");
 const inputEl = document.querySelector(".input");
 const headingEl = document.querySelector(".heading");
 
+const number2 = 10;
+const user2 = {
+  name: "didoki",
+  city: "Syngapore",
+};
+
 inputEl.value = "Testing input";
-headingEl.innerHTML = "Javascript <span>DOM element </span> ";
+headingEl.insertAdjacentHTML(
+  "beforebegin",
+  "Javascript <span>DOM element </span> "
+);
+
+headingEl.innerHTML = `
+Javascript <span>DOM element </span> 
+how is this class ?
+JS is fun :)
+<br/>number: ${number2}
+<br/> user name: ${user2.name}
+`;
 
 buttonEl.blur();
 // buttonEl.focus();
