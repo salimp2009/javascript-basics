3253dc987f894e2e688a7c8f9ca1d0fa28ada35e
salimp2009@gmail.com <salimp2009@gmail.com>
2023-03-28 12:35:26 +0300

    added examples for accessing & manipulating DOM elements

---
+++ script_dom.js
@@ -0,0 +1,10 @@
+const buttonEl = document.querySelector(".btn");
+const inputEl = document.querySelector(".input");
+const headingEl = document.querySelector(".heading");
+
+inputEl.value = "Testing input";
+headingEl.innerHTML = "Javascript <span>DOM element </span> ";
+
+buttonEl.blur();
+// buttonEl.focus();
+// buttonEl.disabled = true;
