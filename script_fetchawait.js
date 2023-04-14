f4b60076d632575d9720adca35230dedd7fe0098
salimp2009@gmail.com <salimp2009@gmail.com>
2023-04-14 13:46:05 +0300

    added sample using await in async functions;

--- script_fetchawait.js
+++ script_fetchawait.js
@@ -1,16 +1,38 @@
 const btnEl = document.querySelector(".btn");
 console.log(btnEl);
+
+// traditional .then for async event; prefer newer method with await
 const clickHandler = () => {
-  console.log(btnEl.textContent);
   fetch("https://reqres.in/api/users")
     .then((response) => {
       // console.log(response);
+      if (!response.ok) {
+        console.log("Something went wrong");
+        return;
+      }
       return response.json();
     })
     .then((data) => {
       console.log(data.data[3].first_name);
       // console.log(JSON.stringify(data));
-    });
+    })
+    .catch((error) => console.log(error));
 };
 
-btnEl.addEventListener("click", clickHandler);
+const clickHandler2 = async () => {
+  try {
+    const response = await fetch("https://reqres.in/api/users");
+    const data = await response.json();
+
+    if (!response.ok) {
+      console.log(data.description);
+      return;
+    }
+    console.log(data.data[3].first_name);
+    // console.log(JSON.stringify(data));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+btnEl.addEventListener("click", clickHandler2);
