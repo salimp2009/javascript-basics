3bc203df6147bd2313041b9e2665bf5e972d1b51
salimp2009@gmail.com <salimp2009@gmail.com>
2023-04-15 22:29:28 +0300

    added simple examples for Math builtin functions and Date object

--- script_imprtexprt.js
+++ script_imprtexprt.js
@@ -24,4 +24,19 @@ switch (browser) {
     console.log("Unknown browser!");
 }
 
+// window is a global object; has functions & elements like
+// alert, fetcth, document, ..
 console.log(window);
+
+// Builtin Math functions
+console.log(Math.ceil(5.9));
+console.log(Math.floor(5.9));
+
+const randNumbr = Math.random() * 100;
+
+console.log("random Number:", randNumbr);
+
+// Date object
+const date = new Date(2053, 2, 27);
+date.setFullYear(2023);
+console.log(date, `${new Date()}`);
