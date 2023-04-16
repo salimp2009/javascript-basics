0cef0abac518f4a91529deb0c769a9cbea17f849
salimp2009@gmail.com <salimp2009@gmail.com>
2023-04-17 00:24:33 +0300

    added old way objected oriented class definition

--- script_imprtexprt.js
+++ script_imprtexprt.js
@@ -40,3 +40,35 @@ console.log("random Number:", randNumbr);
 const date = new Date(2053, 2, 27);
 date.setFullYear(2023);
 console.log(date, `${new Date()}`);
+
+const apartment1 = {
+  sqMeters: 50,
+  numbrOfBedrooms: 3,
+  isBig: function () {
+    return this.sqMeters > 100 ? true : false;
+  },
+};
+
+console.log(apartment1.isBig());
+
+const createApartment = (sqmeters, numberofbedrooms) => {
+  return {
+    sqMeters: sqmeters,
+    numbrOfBedrooms: numberofbedrooms,
+    isBig: function () {
+      return this.sqMeters > 100 ? true : false;
+    },
+  };
+};
+
+console.log(createApartment(100, 4));
+
+class Apartment {
+  constructor(sqMeters, numbrOfBedrooms) {
+    this.sqMeters = sqMeters;
+    this.numbrOfBedrooms = numbrOfBedrooms;
+  }
+}
+
+const apartment2 = new Apartment(50, 4);
+console.log(apartment2);
