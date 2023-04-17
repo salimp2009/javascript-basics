6fcbc08bac5af8e5b577ad8e4a78d5e2bb0d2ccb
salimp2009@gmail.com <salimp2009@gmail.com>
2023-04-17 17:43:52 +0300

    examples of creating/using/consuming Promises

--- script_promises.js
+++ script_promises.js
@@ -1,1 +1,28 @@
 console.log("promises hello!");
+
+const p1 = new Promise((resolve, reject) => {
+  const numbOfClients = 20;
+
+  if (numbOfClients > 10) {
+    resolve("Success");
+  } else {
+    reject("not enough advertising");
+  }
+});
+
+p1.then((value) => {
+  console.log(value);
+}).catch((error) => {
+  console.log(error);
+});
+
+const checkResults = async () => {
+  try {
+    const value = await p1;
+    console.log(value);
+  } catch (reason) {
+    console.log(reason);
+  }
+};
+
+checkResults();
