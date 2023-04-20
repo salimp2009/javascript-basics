bdb598e257abbae7db72e99dab52d61deeef2cf7
salimp2009@gmail.com <salimp2009@gmail.com>
2023-04-20 21:59:45 +0300

    added examples for handling multiple calls with Promise by using Promise.all or any

--- script_promises.js
+++ script_promises.js
@@ -27,7 +27,7 @@ const checkResults = async () => {
 
 checkResults();
 
-fetch("https://reqress.in/api/users")
+fetch("https://reqres.in/api/users")
   .then((response) => {
     if (!response.ok) {
       return Promise.reject("4xx or 5xx problem");
@@ -38,3 +38,16 @@ fetch("https://reqress.in/api/users")
   .catch((error) => {
     console.log("Error is", error);
   });
+
+const bugIcon = "\ueaaf";
+console.log(bugIcon);
+
+// when you have multiple call, wait until all are resolved;
+// Promise.all([fetchPhotoPromise, fetchComments]).then((values) => {
+//   render(values);
+// });
+
+// Handle multiple function and use whichever finishes first
+// Promise.any([fetchPhotoServer1, fetchPhotoServer2]).then((value) => {
+//   render(value);
+// });
